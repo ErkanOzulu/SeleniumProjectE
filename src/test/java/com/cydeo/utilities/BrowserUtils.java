0789ea42a4e7d0aaa5b205801bcd2a88ab9c8548
Erkan <ErkanOzulu@users.noreply.github.com>
2023-01-07 16:11:53 +0300

    day7 BrowserUtils switchWindowAndVerify

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,15 +1,23 @@
 package com.cydeo.utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
+/*
+In this class only general utility methods that are NOT related to some specific page.
+ */
 public class BrowserUtils {
     /*
     This method will accept int (in seconds) and execute Thread.sleep
     for given duration
      */
-    public static void sleep(int second){
-        second *=1000;
+    public static void sleep(int second) {
+        second *= 1000;
         try {
             Thread.sleep(second);
-        }catch (InterruptedException e ) {
+        } catch (InterruptedException e) {
 
         }
     }
@@ -21,5 +29,42 @@ public class BrowserUtils {
         - If condition matches, will break loop.
     Arg3: expectedInTitle to be compared against actualTitle
      */
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInURL,String expectedInTitle ){
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+
+            driver.switchTo().window(each);
+            System.out.println("Current Url(): " + driver.getCurrentUrl());
+            if (driver.getCurrentUrl().contains(expectedInURL)){
+                break;
+            }
+        }
+
+        //5. Assert: Title contains “expectedInTitle”
+
+        String actualTitle=driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
+    /*
+    TC #2: Create utility method
+1. Create a new class called BrowserUtils
+2. Create a method to make Task1 logic re-usable
+3. When method is called, it should switch window and verify title.
+Method info:
+• Name: switchWindowAndVerify
+• Return type: void
+• Arg1: WebDriver
+• Arg2: String expectedInUrl
+• Arg3: String expectedTitle
+     */
+
+
+
+
 
 }
