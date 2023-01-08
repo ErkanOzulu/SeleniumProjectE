c4c111f7e3e8743a3e139b65c6d45d7051300feb
Erkan <ErkanOzulu@users.noreply.github.com>
2023-01-08 17:17:58 +0300

    day8 utils

---
+++ src/test/java/com/cydeo/utilities/WebTableUtils.java
@@ -0,0 +1,72 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WebTableUtils {
+    //Method #1 info:
+    //• Name: returnOrderDate ()
+    //• Return type: String
+    //• Arg1: WebDriver driver
+    //• Arg2: String costumerName
+    //This method should accept a costumerName and return the costumer order date as a String.
+
+    public static String returnOrderDate(WebDriver driver, String costumerName){
+
+        String locator="//td[.='"+costumerName+"']/following-sibling::td[3]";
+        WebElement customerDateCell = driver.findElement(By.xpath(locator));
+
+        return customerDateCell.getText();
+    }
+    //Method #2 info:
+    //• Name: orderVerify ()
+    //• Return type: void
+    //• Arg1: WebDriver driver
+    //• Arg2: String customerName
+    //• Arg3: String expectedOrderDate
+    //This method should accept above mentioned arguments and internally assert
+    //expectedOrderDate matching actualOrderDate.
+
+    public static void orderVerify (WebDriver driver, String customerName,String expectedOrderDate){
+        String locator="//td[.='"+customerName+"']/following-sibling::td[3]";
+        WebElement customerDateCell = driver.findElement(By.xpath(locator));
+
+        String actualOrderDate=customerDateCell.getText();
+        Assert.assertEquals(actualOrderDate,expectedOrderDate);
+    }
+
+
+
+
+
+/*
+    public static String returnOrderDate2(WebDriver driver, String costumerName){
+        return driver.findElement(By.xpath("//td[.='"+costumerName+"']/following-sibling::td[3]")).getText();
+    }
+
+
+ */
+}
+/*
+TC #2: Web table practice Task1 cont.
+1. Create a new class called WebTableUtils.
+2. Create two methods to verify order
+Method #1 info:
+• Name: returnOrderDate ()
+• Return type: String
+• Arg1: WebDriver driver
+• Arg2: String costumerName
+This method should accept a costumerName and return the costumer order date
+as a String.
+Method #2 info:
+• Name: orderVerify ()
+• Return type: void
+• Arg1: WebDriver driver
+• Arg2: String costumerName
+• Arg3: String expectedOrderDate
+This method should accept above mentioned arguments and internally assert
+expectedOrderDate matching actualOrderDate.
+ */
