131498ae8bca3ff087f1ebd3b9d4954cc48482f0
Erkan <ErkanOzulu@users.noreply.github.com>
2023-01-11 17:24:17 +0300

    added switch

--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -15,23 +15,46 @@ public class WebDriverFactory {
 //   - if "chrome" passed --> it will open chrome browser
 //   - if "firefox" passed --> it will open firefox browser
 // RETURN TYPE: "WebDriver"
-    public static WebDriver getDriver(String browserType){
-        if (browserType.equalsIgnoreCase("chrome")){
+    public static WebDriver getDriver(String browserType) {
+        if (browserType.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             return new ChromeDriver();
         } else if (browserType.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             return new FirefoxDriver();
-        }else if (browserType.equalsIgnoreCase("edge")) {
+        } else if (browserType.equalsIgnoreCase("edge")) {
             WebDriverManager.edgedriver().setup();
             return new EdgeDriver();
-        }else {
+        } else {
             System.out.println("Given browser type doesn't exist/or is not currently supported");
             System.out.println("Driver=null");
             return null;
         }
 
+    }
+/*
+    public static WebDriver getDriver1(String browserName){
 
+        WebDriver driver;
+        switch (browserName.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.chromedriver().setup();
+                driver=new FirefoxDriver();
+                break;
+
+            default:
+                System.out.println("Unknown browser type "+browserName);
+                driver=null;
+        }
+        driver.manage().window().maximize();
+
+        return driver;
     }
 
+
+ */
 }
