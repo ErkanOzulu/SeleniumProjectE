bca63ddd22fae9d96294136729df0524ad258073
Erkan <ErkanOzulu@users.noreply.github.com>
2023-01-14 22:04:06 +0300

    day09 test base

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -1,28 +1,33 @@
-package com.cydeo.base;
+package com.cydeo.tests.base;
 
+import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
-public class TestBase {
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
 
     public WebDriver driver;
 
-    /*
+
 
     @BeforeMethod
     public void setupMethod() {
-
-        //1. Open browser
+        //1. Create new test and make set ups
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
+
     @AfterMethod
     public void tearDown(){
         driver.quit();
     }
 
 
-     */
+
 
 }
