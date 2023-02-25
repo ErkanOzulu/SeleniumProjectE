3bdebffa3cf5c29da9621b5b41ba7b29f7651492
Erkan <ErkanOzulu@users.noreply.github.com>
2023-02-25 15:12:14 +0300

    apply POM design pattern

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_staleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_staleElementReferenceException.java
@@ -28,6 +28,13 @@ public class T6_staleElementReferenceException {
         driver.navigate().refresh();
 
         //todo to handle staleElementReferenceException
+
+        /** When we implement POM Design pattern we solve StaleElementReferenceException by default.
+
+        - How it works?
+           - Every time we want to use WebElement located using @FindBy annotation, it will be re-located in the line we are using it.
+          - Basically it will automatically do a "freshness check" which will eliminate the StaleElementReferenceException
+        */
         //We are refreshing the web element reference by re-assigning (re-locating) the web element
         cydeoLink = driver.findElement(By.xpath("//a[@target='_blank']"));
         //5- Verify it is displayed, again.
